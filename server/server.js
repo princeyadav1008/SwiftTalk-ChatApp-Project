97278595719a68f83e2519cf1c09e94a1e8751e1
princeyadav1008 <prince.py1824@gmail.com>
2025-11-05 00:24:39 +0530

    added file

--- server/server.js
+++ server/server.js
@@ -8,45 +8,45 @@ import messageRouter from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
 
 //create Express app and HTTP server
-const app=express();
-const server=http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 
 //Initialize socket.io server
 export const io = new Server(server, {
-  cors: {origin: "*"}
-})
+  cors: { origin: " https://swift-talk-chat-app-project.vercel.app/" },
+});
 
 //Store online users
-export const userSocketMap= {};// {userId: socketId}
+export const userSocketMap = {}; // {userId: socketId}
 
 //Socket.io connection handler
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
-  console.log("User Connected",userId);
+  console.log("User Connected", userId);
 
-  if(userId) userSocketMap[userId]= socket.id;
+  if (userId) userSocketMap[userId] = socket.id;
 
   //Emit online users to all connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-  socket.on("disconnect",()=>{
+  socket.on("disconnect", () => {
     console.log("User Disconnected", userId);
     delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap))
-  })
-})
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 //Middleware setup
-app.use(express.json({limit:"4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Routes setup
-app.use("/api/status" ,(req,res)=>res.send("Server is live"));
-app.use("/api/auth",userRouter);
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 //Connect to MongoDB
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT)); 
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
